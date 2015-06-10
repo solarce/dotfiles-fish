a0b7eb9970c8b26d93f3c806f0a0f4c3117a583f
Brandon Burton <brandon@inatree.org>
2015-06-10 15:49:21 -0700

    adding some git info to my prompt

--- config.fish
+++ config.fish
@@ -5,6 +5,10 @@ for alias_file in (ls $HOME/.config/fish/aliases/*.fish)
   source $alias_file
 end
 
+for prompt_helper in (ls $HOME/.config/fish/prompt_helpers/*.fish)
+  source $prompt_helper
+end
+
 # Interactive/login shells {{{
 #
 if status --is-login
